e01f07506b58a9b6633dbefc2717de180e39bb01
bharker <38762780+bharker@users.noreply.github.com>
2018-04-26 19:21:18 +0000

    Add files via upload

---
+++ dbo.TEST.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE TEST
+
+@bor_user varchar(30),
+@bor_password varchar(30)
+
+as
+
+select bor_user, bor_password from tblBorrower where  bor_user = @bor_user and bor_password = @bor_password
